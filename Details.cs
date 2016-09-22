c64cf00c3c2b0a39f52f97b888187c148c345cc6
GauthamPughaz <gauthamleo55@gmail.com>
2016-09-23 01:39:38 +0530

    Add files via upload
    I have pushed a model file and controller file with dummy array value. I will replace the dummy data once the database is ready.

---
+++ Details.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Details
+    {
+        public String ID { get; set; }
+        public String password { get; set; }
+        
+    }
+}
