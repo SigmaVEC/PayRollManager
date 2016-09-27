a4814ce80b3f67f2dbd6792f1c617a06bf404416
Rahul Sriram <rahulrockingsriram@gmail.com>
2016-09-27 14:35:07 +0530

    changed default reply format to JSON from XML

--- PayRollManager/App_Start/WebApiConfig.cs
+++ PayRollManager/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace PayRollManager
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html") );
         }
     }
 }
