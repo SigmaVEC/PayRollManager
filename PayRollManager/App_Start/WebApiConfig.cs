82e2d3dc8656c750a447e99e0fdbc551e089f3c5
RahulSriram <rahulrockingsriram@gmail.com>
2016-09-28 22:43:34 +0530

    database interfacing done, authenticate controller working

--- PayRollManager/App_Start/WebApiConfig.cs
+++ PayRollManager/App_Start/WebApiConfig.cs
@@ -19,8 +19,9 @@ namespace PayRollManager
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html") );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
---
+++ PayRollManager/Controllers/AuthenticateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PayRollManager.Models;
+
+namespace PayRollManager.Controllers {
+    public class AuthenticateController : ApiController {
+        private PayRollManagerEntities db = new PayRollManagerEntities();
+
+        // POST: api/Authenticate
+        [ResponseType(typeof(Company_Details))]
+        [HttpGet]
+        public IHttpActionResult Auth(String companyName, String id, String password) {
+            var res = db.Company_Details.FirstOrDefaultAsync((p) => (p.CompanyName == companyName && p.EmployeeId == id && p.Password == password));
+            return Ok(res.Result);
+        }
+    }
+}
