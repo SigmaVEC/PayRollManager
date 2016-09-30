d405ed200fa419ad2f44212c5a0f67e8f1480242
RahulSriram <rahulrockingsriram@gmail.com>
2016-09-30 14:12:30 +0530

    Employee View added

---
+++ PayRollManager/Controllers/EmployeeViewController.cs
@@ -0,0 +1,41 @@
+﻿using PayRollManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace PayRollManager.Controllers {
+    public class EmployeeViewController : ApiController {
+        private PayRollManagerEntities db = new PayRollManagerEntities();
+
+        // GET: api/EmployeeView
+        [ResponseType(typeof(Employee_Info))]
+        [HttpGet]
+        public IHttpActionResult View(int companyId, String employeeId, String password) {
+            var company = db.Company_Info.FirstOrDefaultAsync((p) => (p.CompanyId == companyId));
+
+            if (company.Result != null) {
+                var employee = db.Employee_Info.FirstOrDefaultAsync((p) => (p.EmployeeId == employeeId && p.Password == password));
+
+                if (employee.Result != null) {
+                    EmployeeViewModel ev = new EmployeeViewModel {
+                        id = employee.Result.EmployeeId,
+                        name = employee.Result.EmployeeName,
+                        doj = employee.Result.DOJ,
+                        salary = db.Employee_Salary.Where((p) => (p.CompanyId == companyId && p.EmployeeId == employeeId)).ToArray()
+                    };
+
+                    return Ok(ev);
+                } else {
+                    return Ok(employee.Result);
+                }
+            } else {
+                return Ok(company.Result);
+            }
+        }
+    }
+}
---
+++ PayRollManager/Models/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayRollManager.Models {
+    public class EmployeeViewModel {
+        public String id { get; set; }
+        public String name { get; set; }
+        public DateTime doj { get; set; }
+        public Employee_Salary[] salary { get; set; }
+    }
+}
