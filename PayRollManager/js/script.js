d7717e01f063a3413fdb3b945850376f67449fd0
shivakishore14 <shivakishore14@gmail.com>
2016-10-03 22:00:22 +0530

    Implemented login Ui and Display company

---
+++ PayRollManager/js/script.js
@@ -0,0 +1,22 @@
+﻿function setCookie(value) {
+    var d = new Date();
+    d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "Token=" + value + "; " + expires + "; path=/";
+}
+function getCookie() {
+    //console.log("get Cookie called");
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf("Token=") == 0) {
+            c1 = c.split("=");
+            return c1[1];
+            // return c.substring(name.length,name.length+ c.length);
+        }
+    }
+    return "";
+}
