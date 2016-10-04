af503b27f3ce64e3882dd2f97ab0b1c5522ea289
shivakishore14 <shivakishore14@gmail.com>
2016-10-04 16:41:54 +0530

    signout, username and cookie changes

--- PayRollManager/js/script.js
+++ PayRollManager/js/script.js
@@ -4,15 +4,18 @@
     var expires = "expires=" + d.toUTCString();
     document.cookie = "Token=" + value + "; " + expires + "; path=/";
 }
-function getCookie() {
+function getCookie(a) {
     //console.log("get Cookie called");
     var ca = document.cookie.split(';');
+    console.log(ca);
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) === " ") {
+            //console.log(c);
             c = c.substring(1);
+            //console.log(c);
         }
-        if (c.indexOf("Token=") == 0) {
+        if (c.indexOf(a) == 0) {
             c1 = c.split("=");
             return c1[1];
             // return c.substring(name.length,name.length+ c.length);
@@ -20,3 +23,26 @@ function getCookie() {
     }
     return "";
 }
+function deleteCookie() {
+    var d = new Date();
+    d.setTime(d.getTime() + (-2 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "Token=null" + "; " + expires + "; path=/";
+}
+function setUserName(value) {
+    var d = new Date();
+    d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "Uname=" + value + "; " + expires + "; path=/";
+}
+function notify(s) {
+    $("#notificationArea").html(s);
+    $('#notification').slideDown(300).delay(3000).slideUp(300);
+}
+document.write(`<div id="notification" class="container-fluid">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4" id="notificationArea"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>`);
