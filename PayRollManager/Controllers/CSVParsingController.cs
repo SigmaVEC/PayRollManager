b11a5ba073ace6417ce773d0ba036d09b21f67b3
GauthamPughaz <gauthamleo55@gmail.com>
2016-10-16 18:12:45 +0530

    Modified CSVParsingController and Bugs fixed.

--- PayRollManager/Controllers/CSVParsingController.cs
+++ PayRollManager/Controllers/CSVParsingController.cs
@@ -43,55 +43,69 @@ namespace PayRollManager.Controllers
             if(session!=null)
             {
                 // Get the file's text.
-                string whole_file = System.IO.File.ReadAllText(path);
-
-                // Split into lines.
-                whole_file = whole_file.Replace('\n', '\r');
-                string[] lines = whole_file.Split(new char[] { '\r' },
-                    StringSplitOptions.RemoveEmptyEntries);
-
-                // See how many rows and columns there are.
-                int num_rows = lines.Length;
-                int num_cols = lines[0].Split(',').Length;
-
-                // Allocate the data array.
-                string[,] values = new string[num_rows, num_cols];
-
-                // Load the array.
-                for (int r = 0; r < num_rows; r++)
+                try
                 {
-                    string[] line_r = lines[r].Split(',');
-                    for (int c = 0; c < num_cols; c++)
+
+                    string whole_file = System.IO.File.ReadAllText(path);
+
+                    // Split into lines.
+                    whole_file = whole_file.Replace('\n', '\r');
+                    string[] lines = whole_file.Split(new char[] { '\r' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    // See how many rows and columns there are.
+                    int num_rows = lines.Length;
+                    int num_cols = lines[0].Split(',').Length;
+
+                    // Allocate the data array.
+                    string[,] values = new string[num_rows, num_cols];
+
+                    // Load the array.
+                    for (int r = 0; r < num_rows; r++)
                     {
-                        values[r, c] = line_r[c];
+                        string[] line_r = lines[r].Split(',');
+                        for (int c = 0; c < num_cols; c++)
+                        {
+                            values[r, c] = line_r[c];
+                        }
                     }
+
+                    var dictionaryTransformed = new List<Dictionary<string, string>>();
+                    var jsondict = new List<string>();
+                    for (int r = 1; r < 3; r++)
+                        for (int r = 1; r < num_rows; r++)
+                        {
+                            dictionaryTransformed.Add(new Dictionary<string, string>());
+
+
+
+                            dictionaryTransformed[r - 1].Add(values[0, 0], values[r, 0]);
+                            dictionaryTransformed[r - 1].Add(values[0, 1], values[r, 1]);
+                            dictionaryTransformed[r - 1].Add(values[0, 2], values[r, 2]);
+                            jsondict.Add(Newtonsoft.Json.JsonConvert.SerializeObject(dictionaryTransformed[r - 1]));
+
+
+
+                        }
+                    var result = new Dictionary<string, List<string>>();
+                    result["attendance"] = jsondict;
+                    var jsonresult = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+                    return Ok(new Message
+                    {
+                        data = json,
+                        message = "Success"
+                    });
+                }
+                catch
+                {
+                    return Ok(new Message
+                    {
+                        data = null,
+                        message = "Cannot open File."
+                    });
+
                 }
 
-                var dictionaryTransformed = new List<Dictionary<string,string>>();
-                var jsondict = new List<string>();
-                for (int r = 1; r < 3; r++)
-                    for (int r=1;r<num_rows;r++)
-                {
-                    dictionaryTransformed.Add(new Dictionary<string, string>());
-
-
-
-                    dictionaryTransformed[r - 1].Add(values[0, 0], values[r, 0]);
-                    dictionaryTransformed[r - 1].Add(values[0, 1], values[r, 1]);
-                    dictionaryTransformed[r - 1].Add(values[0, 2], values[r, 2]);
-                    jsondict.Add(Newtonsoft.Json.JsonConvert.SerializeObject(dictionaryTransformed[r - 1]));
-
-
-
-                }
-                var result = new Dictionary<string, List<string>>();
-                result["attendance"] = jsondict;
-                var jsonresult=Newtonsoft.Json.JsonConvert.SerializeObject(result);
-                return Ok(new Message
-                {
-                    data = json,
-                    message = "Success"
-                });
 
             }
         }
