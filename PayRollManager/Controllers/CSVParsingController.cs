3d852ed0e56f988e3c5266000ac3e01d97efb6bc
GauthamPughaz <gauthamleo55@gmail.com>
2016-10-19 18:55:20 +0530

    Added index.cshtml for upload and modified csv parser and added upload controller

--- PayRollManager/Controllers/CSVParsingController.cs
+++ PayRollManager/Controllers/CSVParsingController.cs
@@ -1,73 +1,37 @@
-﻿using PayRollManager.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using PayRollManager.Models;
 
 namespace PayRollManager.Controllers
 {
-    public class CSVParsingController : ApiController
+    public class CSVParserController : ApiController
     {
         private PayRollManagerEntities db = new PayRollManagerEntities();
-        string path;
-        // POST: api/Index
-        [HttpPost]
-        public IHttpActionResult Index(HttpPostedFileBase file)
-        {
-            if (file != null && file.ContentLength > 0)
-                try
-                {
-                    path = Path.Combine(HttpContext.Current.Server.MapPath("~/Files"),
-                                               Path.GetFileName(file.FileName));
-                    file.SaveAs(path);
-                    return Ok(new Message
-                    {
-                        data = null,
-                        message = "Success"
-                    });
-
-                    
-                }
-                catch (Exception ex)
-                {
-                    return Ok(new Message
-                    {
-                        data = null,
-                        message = "Error in Uploading"
-                    });
-                }
-            else
-            {
-                return Ok(new Message
-                {
-                    data = null,
-                    message = "You have not specified a file."
-                });
-            }
-            
-        }
-
-       
 
         // GET: api/CSVParser
         [HttpGet]
-        public IHttpActionResult CSVParser(String token)
+        public IHttpActionResult CSVParser(string token, string path)
         {
             var tokenLifetime = int.Parse(ConfigurationManager.AppSettings["tokenLifetime"]);
             var session = db.Session_Tokens.FirstOrDefault((p) => (p.SessionToken == token && DbFunctions.DiffHours(DateTime.Now, p.Timestamp) < tokenLifetime));
-            if(session!=null)
+            if (session != null)
             {
                 // Get the file's text.
                 try
                 {
 
-                    string whole_file = System.IO.File.ReadAllText(path);
+                    string whole_file = File.ReadAllText(path);
 
                     // Split into lines.
                     whole_file = whole_file.Replace('\n', '\r');
@@ -93,24 +57,25 @@ namespace PayRollManager.Controllers
 
                     var dictionaryTransformed = new List<Dictionary<string, string>>();
                     var jsondict = new List<string>();
-                    
-                        for (int r = 1; r < num_rows; r++)
-                        {
-                            dictionaryTransformed.Add(new Dictionary<string, string>());
+
+                    for (int r = 1; r < num_rows; r++)
+                    {
+                        dictionaryTransformed.Add(new Dictionary<string, string>());
 
 
 
-                            dictionaryTransformed[r - 1].Add(values[0, 0], values[r, 0]);
-                            dictionaryTransformed[r - 1].Add(values[0, 1], values[r, 1]);
-                            dictionaryTransformed[r - 1].Add(values[0, 2], values[r, 2]);
-                            jsondict.Add(Newtonsoft.Json.JsonConvert.SerializeObject(dictionaryTransformed[r - 1]));
+                        dictionaryTransformed[r - 1].Add(values[0, 0], values[r, 0]);
+                        dictionaryTransformed[r - 1].Add(values[0, 1], values[r, 1]);
+                        dictionaryTransformed[r - 1].Add(values[0, 2], values[r, 2]);
+                        jsondict.Add(Newtonsoft.Json.JsonConvert.SerializeObject(dictionaryTransformed[r - 1]));
 
 
 
-                        }
+                    }
                     var result = new Dictionary<string, List<string>>();
                     result["attendance"] = jsondict;
                     var jsonresult = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+                    Trace.WriteLine(jsonresult);
                     return Ok(new Message
                     {
                         data = jsonresult,
@@ -140,3 +105,4 @@ namespace PayRollManager.Controllers
         }
     }
 }
+
---
+++ PayRollManager/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace PayRollManager.Controllers
+{
+    public class UploadController : ApiController
+    {
+
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            Trace.WriteLine(root);
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the file names.
+                var path = "";
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    //To View your file location in server.
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    path = file.LocalFileName;
+
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,path);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
